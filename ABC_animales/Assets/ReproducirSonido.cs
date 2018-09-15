4a262812e4f443e8580a07d9ca32d813436b0bfb
Jose Saldaña <josesaldanavi@gmail.com>
2018-09-15 01:31:07 -0500

    test -cow walk(button)

---
+++ ABC_animales/Assets/ReproducirSonido.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproducirSonido : MonoBehaviour {
+
+	public void Reproducir()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ ABC_animales/Assets/Testeo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testeo : MonoBehaviour {
+    public Animator anim;
+    int runState;
+    public bool run;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Activar();
+	}
+
+    public void Activar()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Go");
+        }
+        else
+        {
+            run=false;
+        }
+    }
+}
